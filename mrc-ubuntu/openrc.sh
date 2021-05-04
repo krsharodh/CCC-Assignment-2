8f6217dc53fd908d37afdac60fa2708cc2cf281e
Chris Xiu <xiuchengjun@gmail.com>
2021-05-04 11:28:04 +1000

    added docker and couchdb

--- mrc-ubuntu/openrc.sh
+++ mrc-ubuntu/openrc.sh
@@ -24,12 +24,12 @@ unset OS_TENANT_ID
 unset OS_TENANT_NAME
 # In addition to the owning entity (tenant), OpenStack stores the entity
 # performing the action as the **user**.
-export OS_USERNAME="jiarui.chen@student.unimelb.edu.au"
+export OS_USERNAME="chris.xiu@student.unimelb.edu.au"
 # With Keystone you pass the keystone password.
 echo "Please enter your OpenStack Password for project $OS_PROJECT_NAME as user $OS_USERNAME: "
 
-export OS_PASSWORD="MjAzZWFkODQ0ZDM3MzY0"
-echo "Password: MjAzZWFkODQ0ZDM3MzY0"
+export OS_PASSWORD="M2E1MTFmNWU3NTg0YjEz"
+echo "Password: M2E1MTFmNWU3NTg0YjEz"
 
 # If your configuration has multiple regions, we set that information here.
 # OS_REGION_NAME is optional and only valid in certain environments.
