873b2bbb0d07c9ba75b7d42333a14c86ed62470c
Sharodh K R <61955784+krsharodh@users.noreply.github.com>
2021-05-19 15:02:25 +1000

    enabling cors

--- Flask-Service/main.py
+++ Flask-Service/main.py
@@ -2,10 +2,21 @@ from flask import Flask, g, request
 from flask_restful import Resource, Api, request
 from flask_cors import CORS
 import couchdb
+import requests
+import json
 
 app = Flask(__name__)
-# cors = CORS(app)
+cors = CORS(app)
+# cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 api = Api(app)
+
+
+user = "admin"
+password = "admin"
+
+
+def get_view(db_name, doc_name, view_name, group_level):
+    return requests.get(f"http://{user}:{password}@172.26.133.161:5984/{db_name}/_design/{doc_name}/_view/{view_name}?reduce=true&group_level={group_level}")
 
 
 class Cities(Resource):
@@ -35,32 +46,60 @@ class Sample(Resource):
 
 class CovidGraph1(Resource):
     def get(self):
-        return ([
-            {"name": "Melbourne", "remaining": 1000, "covid": 335},
-            {"name": "Brisbane", "remaining": 1000, "covid": 345},
-            {
-                "name": "Perth",
-                "remaining": 1000,
-                "covid": 2110
-            },
-            {
-                "name": "Adelaide",
-                "remaining": 1000,
-                "covid": 540
-            },
-            {
-                "name": "Hobart",
-                "remaining": 1000,
-                "covid": 335
-            },
-            {"name": "Darwin", "remaining": 1000, "covid": 110},
-            {"name": "Canberra", "remaining": 1000, "covid": 110}
-        ])
+
+        data = []
+
+        # Count the number of tweets mentioned covid in each city
+        covid_tweet_city = json.loads(
+            get_view(
+                db_name="raw_tweets_from_timeline",
+                doc_name="covid_related",
+                view_name="CityDateTime_count",
+                group_level=1
+            ).content.decode("utf-8"))
+
+        for row in covid_tweet_city['rows']:
+            # print(row["key"], row["value"])
+            data.append({
+                "city": row["key"][0],
+                "metioned_covid": row["value"]
+            })
+
+        # Count the total number of tweets in each city
+        tweet_city = json.loads(
+            get_view(
+                db_name="raw_tweets_from_timeline",
+                doc_name="covid_related",
+                view_name="Tweet_count",
+                group_level=1
+            ).content.decode("utf-8"))
+
+        for row in tweet_city['rows']:
+            # print(row["key"], row["value"])
+            for i in range(len(data)):
+                if row["key"] == data[i]["city"]:
+                    data[i]["total_tweets"] = row["value"]
+
+        # print(data)
+
+        return data
 
 
 class CovidGraph2(Resource):
     def get(self):
         print(request.args['city'])
+
+        covid_tweet_citymonth = json.loads(
+            get_view(
+                db_name="raw_tweets_from_timeline",
+                doc_name="covid_related",
+                view_name="CityDateTime_count",
+                group_level=3
+            ).content.decode("utf-8"))
+
+        for row in covid_tweet_citymonth['rows']:
+            print(row["key"], row["value"])
+
         return ([
             {
                 "name": "1/01/2021",
