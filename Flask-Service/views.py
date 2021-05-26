7cde46698f40b44fd2d0d99b47670869b427286d
yinilai <yinilai1021@gmail.com>
2021-05-26 10:03:32 +1000

    Added views

--- Flask-Service/views.py
+++ Flask-Service/views.py
@@ -44,6 +44,39 @@ map_fun_CityDateTime = '''function (doc) {
 
 reduce_fun_CityDateTime = "_count"
 
+
+map_fun_Wordcloud_covid = '''function(doc){
+    var full_text = doc['full_text'].toLowerCase()
+      if (full_text.indexOf('covid') != -1 || full_text.indexOf('corona virus') != -1 || full_text.indexOf('coronavirus') != -1)
+         var words = full_text.split(' ')
+         for (var i = 0; i < words.length;i++) {
+           var word = words[i]
+           word = word.replace(/[\ |\~|\`|\!|\@|\#|\$|\%|\^|\&|\*|\(|\)|\-|\_|\+|\=|\||\\|\[|\]|\{|\}|\;|\:|\"|\'|\,|\.|\>|\/|\?]/g,"");
+           var regex =/^[0-9A-Za-z]+$/;
+           if (regex.test(word) && 2 <= word.length && word.length <= 20) {
+             emit(word,1)
+           }
+         }
+    }
+'''
+
+reduce_fun_Wordcloud_covid = "_count"
+
+
+map_fun_Wordcloud_hastag_covid = '''function(doc) {
+      var full_text = doc['full_text'].toLowerCase()
+      if (full_text.indexOf('covid') != -1 || full_text.indexOf('corona virus') != -1 || full_text.indexOf('coronavirus') != -1)
+         for (var i = 0; i < doc.hashtags.length; i++) {
+          var hashtag = doc.hashtags[i].text.toLowerCase();
+          if (2 <= hashtag.length && hashtag.length <= 25) {
+            emit(hashtag, 1) 
+          }
+        }
+    }
+'''
+
+reduce_fun_Wordcloud_hastag_covid = "_count"
+
 data_covid = {
         "_id": f"_design/covid_related",
         "views": {
@@ -54,6 +87,14 @@ data_covid = {
             "CityDateTime_count": {
                 "map": map_fun_CityDateTime,
                 "reduce": reduce_fun_CityDateTime
+            },
+            "Worldcloud_covid":{
+                "map": map_fun_Wordcloud_covid,
+                "reduce": reduce_fun_Wordcloud_covid
+            },
+            "Worldcloud_hastag_covid":{
+                "map": map_fun_Wordcloud_hastag_covid,
+                "reduce": reduce_fun_Wordcloud_hastag_covid
             }
         },
         "language": "javascript",
@@ -86,6 +127,60 @@ map_fun_Vaccine_CityDateTime = '''function (doc) {
 
 reduce_fun_Vaccine_CityDateTime = "_count"
 
+
+map_fun_Vaccine_tweets = '''function(doc) {
+      var full_text = doc['full_text'].toLowerCase()
+      if (full_text.indexOf('vaccine') != -1 || full_text.indexOf('vaccinat') != -1 || 
+          full_text.indexOf('pfizer') != -1 || full_text.indexOf('astrazeneca') != -1)
+          emit(doc._id, [doc.created_at, doc.uniform_location, full_text]);
+    }
+'''
+
+
+map_fun_Vaccine_tweets_id = '''function(doc) {
+      var full_text = doc['full_text'].toLowerCase()
+      if (full_text.indexOf('vaccine') != -1 || full_text.indexOf('vaccinat') != -1 || 
+          full_text.indexOf('pfizer') != -1 || full_text.indexOf('astrazeneca') != -1)
+          emit(doc._id, null);
+    }
+'''
+
+reduce_fun_Vaccine_tweets_id = "_count"
+
+
+map_fun_Wordcloud_hashtag_vaccine = '''function(doc) {
+      var full_text = doc['full_text'].toLowerCase()
+      if (full_text.indexOf('vaccine') != -1 || full_text.indexOf('vaccinat') != -1 || 
+          full_text.indexOf('pfizer') != -1 || full_text.indexOf('astrazeneca') != -1)
+         for (var i = 0; i < doc.hashtags.length; i++) {
+          var hashtag = doc.hashtags[i].text.toLowerCase();
+          emit(hashtag, 1)
+    }
+}
+'''
+
+reduce_fun_Wordcloud_hashtag_vaccine = "_count"
+
+
+map_fun_Wordcloud_vaccine = '''function(doc) {
+      var full_text = doc['full_text'].toLowerCase()
+      if (full_text.indexOf('vaccine') != -1 || full_text.indexOf('vaccinat') != -1 || 
+          full_text.indexOf('pfizer') != -1 || full_text.indexOf('astrazeneca') != -1)
+         var words = full_text.split(' ')
+         for (var i = 0; i < words.length;i++) {
+           var word = words[i]
+           word = word.replace(/[\ |\~|\`|\!|\@|\#|\$|\%|\^|\&|\*|\(|\)|\-|\_|\+|\=|\||\\|\[|\]|\{|\}|\;|\:|\"|\'|\,|\.|\>|\/|\?]/g,"");
+           var regex =/^[0-9A-Za-z]+$/;
+           if (regex.test(word) && 2 <= word.length && word.length <= 20) {
+             emit(word,1)
+           }
+         }
+    }
+'''
+
+reduce_fun_Wordcloud_vaccine = "_count"
+
+
 data_vaccine = {
         "_id": f"_design/vaccine_related",
         "views": {
@@ -93,7 +188,21 @@ data_vaccine = {
                 "map": map_fun_Vaccine_CityDateTime,
                 "reduce": reduce_fun_Vaccine_CityDateTime
             },
-            
+            "Vaccine_tweets":{
+                "map": map_fun_Vaccine_tweets
+            },
+            "Vaccine_tweets_id":{
+                "map": map_fun_Vaccine_tweets_id,
+                "reduce": reduce_fun_Vaccine_tweets_id
+            },
+            "Wordcloud_hashtag_vaccine":{
+                "map": map_fun_Wordcloud_hashtag_vaccine,
+                "reduce": reduce_fun_Wordcloud_hashtag_vaccine
+            },
+            "Wordcloud_vaccine":{
+                "map": map_fun_Wordcloud_vaccine,
+                "reduce": reduce_fun_Wordcloud_vaccine
+            }
         },
         "language": "javascript",
         "options": {"partitioned": False}
@@ -124,6 +233,7 @@ map_fun_job_CityDateTime = '''function (doc) {
 '''
 
 reduce_fun_job_CityDateTime = "_count"
+
 
 data_job = {
         "_id": f"_design/job_related",
